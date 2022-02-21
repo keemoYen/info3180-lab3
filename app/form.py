4054c008b360279611b48579d2ff3d42b0bc43fb
Keemo <Keemoyen4@gmail.com>
2022-02-20 21:31:27 -0500

    info3180 update

--- app/form.py
+++ app/form.py
@@ -5,6 +5,6 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 
 class ContactForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired()])
-    subject = StringField('subject', validators=[DataRequired()])
+    name = StringField('Please enter your full name', validators=[DataRequired()])
+    email = StringField('please enter your e-mail address', validators=[DataRequired()])
+    subject = StringField('please enter the subject of your message', validators=[DataRequired()])
